415783430b5c02f835b3992a2fc218b7f7305d60
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-03-23 18:18:21 -0600

    added signup route and login hash passwords

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryp-nodejs');
 
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
@@ -15,7 +16,7 @@ router.get('/getScriptureByID', getScriptureByID);
 
 router.post('/logout', logout);
 router.post('/login', login);
-
+router.post('/signup', signup);
 router.get('/notification', function(req, resp){
 	resp.sendFile('notification.html', {root:__dirname});
 })
@@ -43,18 +44,60 @@ function logout(req,res){
   	}
 }
 
+function signup(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	var telephone = req.body.telephone;
+
+	var hashedPassword = bcrypt.hashSync(password);
+	var sql = 'INSERT INTO scripture.scripture(username, password, phone) VALUES $1::text, $2::text, $3::text';
+	var params = [username, hashedPassword, telephone];
+	pool.query(sql, params, function(err, data){
+		if(err){
+			res.status(400).send("Error: " + err);
+		}
+		else{
+			res.status(204).send();
+		}
+	})
+}
+
 function login(req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
-	console.log(username + " " + password);
-	if(username == 'admin' && password == 'password'){
-  		req.session.username = username;
-  		req.session.views = 2;
-  		res.json({success:true});
-  	}
-  	else{
-  		res.json({success:false});
-  	}
+	var sql = 'SELECT id, username, password FROM scripture.user WHERE username=$1::text';
+	var params = [username];
+	pool.query(sql, params, function (err, data){
+		if(err){
+			//authentication error
+			res.status(400).send("An unkown error occurred");
+		}
+		else {
+			bcrypt.compare(password, data.rows[0]['password'], function(err, result){
+				if(!result){
+					//issue with password or username
+					res.status(401).send("The username or password is incorrect");
+				}
+				else {
+					var id = data.rows[0]['id'];
+					var username = data.rows[0]['username'];
+					//begin session and return data
+					req.session.userid = id;
+					req.session.username = username;
+					res.status(200).send({id: id, username: username, success: true});
+				}
+			})
+		}
+	}) 
+	// console.log(username + " " + password);
+	// if(username == 'admin' && password == 'password'){
+ //  		req.session.username = username;
+ //  		req.session.views = 2;
+ //  		res.json({success:true});
+ //  	}
+ //  	else{
+ //  		res.json({success:false});
+ //  	}
 }
 
 function getUser(req, res){
