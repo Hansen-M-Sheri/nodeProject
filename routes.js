0811bbf5b648c29a03609654193f816d263f8782
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2019-03-29 13:08:29 -0600

    testing syntax to isolate number returned

--- routes.js
+++ routes.js
@@ -217,7 +217,7 @@ function getNumScripturesByTopicID(req, res){
 
 		console.log("Found result: " + JSON.stringify(result.rows));
 		var json = JSON.stringify(result.rows['count']);
-		res.status(200).send(json['count']);
+		res.status(200).send(json['0'].count);
 	})
 }
 function getRandomScriptureIDByTopicID(req, res){
